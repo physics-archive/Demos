692fce8416cf94ff7a03d263d1c9674f1410f0c9
physics-archive <windows.client>
2024-03-20 15:33:43 -0400

    brokenc
    Former-commit-id: 338ceaa5e074546a9feb1a491a95cb2880486466

---
+++ C/runes/stringsearch.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int string_in(const char* string, const char** strings, size_t strings_num)
+{
+    for (size_t i = 0; i < strings_num; i++)
+    {
+        if (!strcmp(string, strings[i]))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
