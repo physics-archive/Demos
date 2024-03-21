f2a4292ab90d642b08038a6cdd65223eae9ce72d
physics-archive <windows.client>
2024-03-20 20:26:08 -0400

    prod.progress
    Former-commit-id: 4364891abc7e1f917dea24f6eea67ccf98413374

---
+++ C/runes/legacy/runes2.backup.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+
+    ///////////////////////////
+    //  Initialize Variables //
+    ///////////////////////////
+    
+    int exponentBase = 3;
+    int exponentPow = 0;
+
+    ///////////////////////////
+    //      String Array     //
+    ///////////////////////////
+
+    char *runeTable[] = { "El","Eld", "Tir", "Nef", "Eth", "Ith", "Tal",
+    "Ral", "Ort", "Thul", "Amn","Sol","Shael","Dol","Hel","Io","Lum",
+    "Ko","Fal","Lem","Pul","Um","Mal","Ist","Gul","Vex","Ohm","Lo","Sur",
+    "Ber","Jah","Cham","Zod",NULL};
+
+    printf("\n%s and sizeof is: %d\n", runeTable[4], sizeof(runeTable[4]));
+
+    char runeCompare[7] = { };
+    printf("%s", "Enter your desired rune: ");
+    fgets(runeCompare, 7, stdin);
+    runeCompare[7] = "NULL"
+    //char drune[7] = { };
+    //strcpy(drune, runeCompare);
+
+    printf("Strncmp Result between % s and %s is: %d", runeCompare, runeTable[1], strncmp(runeCompare, runeTable[1], 5));
+    printf("\n\nSizeof runeCompare(stdin): %d", sizeof(runeCompare));
+    printf("\nSizeof runeTable(Eld): %d\n", sizeof(runeTable[1]));
+
+    printf("%c versus %c\n", runeTable[2], runeCompare[2]);
+    printf("\n%s", drune);
+
+    //string_in()
+    //https://stackoverflow.com/questions/18806788/comparing-a-single-string-to-an-array-of-strings-in-c
+}
+
+//Problem: Strncmp behaving poorly. In array[j][k], high k will cause change in j. Actually this isn't true...
--- C/runes/legacy/runes2c.c
+++ C/runes/legacy/runes2c.c
@@ -10,34 +10,50 @@ int main() {
     
     int exponentBase = 3;
     int exponentPow = 0;
+    int compareString[33]; = { };
 
     ///////////////////////////
     //      String Array     //
     ///////////////////////////
 
-    char *runeTable[] = { "El","Eld", "Tir", "Nef", "Eth", "Ith", "Tal",
+    char runeTable[33][6] = {"El","Eld", "Tir", "Nef", "Eth", "Ith", "Tal",
     "Ral", "Ort", "Thul", "Amn","Sol","Shael","Dol","Hel","Io","Lum",
     "Ko","Fal","Lem","Pul","Um","Mal","Ist","Gul","Vex","Ohm","Lo","Sur",
-    "Ber","Jah","Cham","Zod",NULL};
+    "Ber","Jah","Cham","Zod"};
+    
+    char runeCompare[8] = { }; // Limits to 7 byte string of characters
+    printf("%s", "Desired rune: "); 
+    fgets(runeCompare, 8, stdin); // Gets user input for a fixed-size string
+    runeCompare[strlen(runeCompare) - 1] = 0; // Trims 'Enter' artefact from runeCompare. Result of fgets.
 
-    printf("\n%s and sizeof is: %d\n", runeTable[4], sizeof(runeTable[4]));
+    ///////////////////////////
+    //     String Compare    //
+    ///////////////////////////
 
-    char runeCompare[7] = { };
-    printf("%s", "Enter your desired rune: ");
-    fgets(runeCompare, 7, stdin);
-    runeCompare[7] = "NULL"
-    //char drune[7] = { };
-    //strcpy(drune, runeCompare);
+    for (int j = 0; j <= sizeof(runeTable) / sizeof(runeTable[0]) - 1; j = j + 1)
+    {
+        compareString[j] = strncmp(runeTable[j], runeCompare, 6);
+        //printf("Strncmp for runeTable (%s) vs stdin (%s): %d\n",runeTable[j],runeCompare,compareString);
+        
+        if (compareString == 0)
+        {
 
-    printf("Strncmp Result between % s and %s is: %d", runeCompare, runeTable[1], strncmp(runeCompare, runeTable[1], 5));
-    printf("\n\nSizeof runeCompare(stdin): %d", sizeof(runeCompare));
-    printf("\nSizeof runeTable(Eld): %d\n", sizeof(runeTable[1]));
-
-    printf("%c versus %c\n", runeTable[2], runeCompare[2]);
-    printf("\n%s", drune);
-
-    //string_in()
-    //https://stackoverflow.com/questions/18806788/comparing-a-single-string-to-an-array-of-strings-in-c
+        }
+    
+    
+    }
+    
+    //printf("%d", strlen(runeCompare));
+    
+ 
 }
 
 //Problem: Strncmp behaving poorly. In array[j][k], high k will cause change in j. Actually this isn't true...
+
+    //printf("Strncmp Result between % s and %s is: %d", runeCompare, runeTable[1], strncmp(runeCompare, runeTable[1], 5));
+    //printf("\n\nSizeof runeCompare(stdin): %d", sizeof(runeCompare));
+    //printf("\nSizeof runeTable(Eld): %d\n", sizeof(runeTable[1]));
+    //printf("%c versus %c\n", runeTable[1][2], runeCompare[2]);
+
+    //string_in()
+    //https://stackoverflow.com/questions/18806788/comparing-a-single-string-to-an-array-of-strings-in-c
