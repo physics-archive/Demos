213fde02903faf813f163df77b4a73361f5f8de9
envy <envy@crabragoon99.org>
2024-03-22 23:13:01 -0400

    runes

--- C/runes/runes.c
+++ C/runes/runes.c
@@ -4,26 +4,25 @@
 
 int main() {
 
+    
     ///////////////////////////
     //  Initialize Variables //
     ///////////////////////////
-    char version[100] = "Alpha Ver. 03.22.24-02";
-
-    double x = 3;
-    double p = 0;
+    
+    char version[100] = "Alpha Ver. 03.22.24-02"; 
+    double p = 0; //2^p or 3^p 
     int compareString[33] = { };
-
+    
     ///////////////////////////
     //      String Array     //
     ///////////////////////////
-    printf("Diablo II Rune Calculator\n%s\n\n",version);
-
+    
+    char runeCompare[8] = { };
     char runeTable[34][6] = {"El","Eld", "Tir", "Nef", "Eth", "Ith", "Tal",
     "Ral", "Ort", "Thul", "Amn","Sol","Shael","Dol","Hel","Io","Lum",
     "Ko","Fal","Lem","Pul","Um","Mal","Ist","Gul","Vex","Ohm","Lo","Sur",
-    "Ber","Jah","Cham","Zod",NULL};
-
-    char runeCompare[8] = { }; // Limits to 7 byte string of characters
+    "Ber","Jah","Cham","Zod"}; //6x34 array of characters. Analog to a vector of strings
+    
     printf("%s", "Desired rune: "); 
     fgets(runeCompare, 8, stdin); // Gets user input for a fixed-size string
     runeCompare[strlen(runeCompare) - 1] = 0; // Trims 'Enter' artefact from runeCompare. Result of fgets.
@@ -31,8 +30,8 @@ int main() {
     ///////////////////////////
     //     String Compare    //
     ///////////////////////////
-    
     //Creates a 1x33 array of values equal to -1,0,1; there exists one arr[j]=0 and corresponds to  the user's input
+    
     for (int j = 0; j <= sizeof(runeTable) / sizeof(runeTable[0]) - 1; j = j + 1)
     {
         compareString[j] = strncmp(runeTable[j], runeCompare, 6);
@@ -41,11 +40,13 @@ int main() {
     ////////////////////////////
     //  Computational Engine  //
     ////////////////////////////
-    
     //Operate conditionally on compareString until an element equal to zero is reached
+    
     int k = 0;
     double product = 0;
-    while (compareString[k] != 0 && compareString >= 33)
+    double product_sifted = 0;
+
+    while (compareString[k] != 0 && k < 33)
     {
         if (k <= 21) { 
             product = pow(x, k + 1); //Iterative exponentiation in base-3. Runes (k) 1 to 22.
@@ -56,7 +57,5 @@ int main() {
         printf("%s: %.0lf\n", runeTable[k], product);
         k++;
     }
-    free(runeTable);
-    free(compareString);
     getch();
 }
