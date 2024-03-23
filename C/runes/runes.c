7da97ce29d40cad1c4ea74dbcc8a253c9a499878
envy <envy@crabragoon99.org>
2024-03-23 00:32:32 -0400

    safeguard

--- C/runes/runes.c
+++ C/runes/runes.c
@@ -11,14 +11,15 @@ int main() {
     
     char version[100] = "Alpha Ver. 03.22.24-03"; 
     double p = 0; //2^p or 3^p 
-    int compareString[33] = { };
+    int compareString[9] = { };
     
     ///////////////////////////
     //      String Array     //
     ///////////////////////////
+    
     printf("Diablo II Rune Calculator\n%s\n\n", version);
 
-    char runeCompare[8] = { };
+    char runeCompare[9] = { }; //Debug why >8 is required. Outputs entire table.
     char runeTable[34][6] = {"El","Eld", "Tir", "Nef", "Eth", "Ith", "Tal",
     "Ral", "Ort", "Thul", "Amn","Sol","Shael","Dol","Hel","Io","Lum",
     "Ko","Fal","Lem","Pul","Um","Mal","Ist","Gul","Vex","Ohm","Lo","Sur",
@@ -26,35 +27,32 @@ int main() {
     
     getUserInput:
     printf("%s", "Desired rune: "); 
-    fgets(runeCompare, 8, stdin); // Gets user input for a fixed-size string
+    fgets(runeCompare, 9, stdin); // Gets user input for a fixed-size string
     runeCompare[strlen(runeCompare) - 1] = 0; // Trims 'Enter' artefact from runeCompare. Result of fgets.
 
     ///////////////////////////
     //     String Compare    //
     ///////////////////////////
     //Creates a 1x33 array of values equal to -1,0,1; there exists one arr[j]=0 and corresponds to  the user's input
-    
+    int count = 0;
+
     for (int j = 0; j <= sizeof(runeTable) / sizeof(runeTable[0]) - 1; j = j + 1)
     {
         compareString[j] = strncmp(runeTable[j], runeCompare, 6);
+        if (compareString[j] == 0)
+        {
+            count++;
+        }
+    }
+    if (count != 1) 
+    {
+        goto getUserInput;
     }
     
     ////////////////////////////
     //  CONDITION: TERMINATE  //
     ////////////////////////////
-    int summ = 0;
-    for (int h = 0; h <= sizeof(runeTable) / sizeof(runeTable[0]) - 1; h = h + 1)
-    {
-        if (compareString[h] == 0)
-        {
-            summ++;
-        }
-    }
-    if (summ == 0)
-    {
-        printf("Invalud rune name.\n\n");
-        goto getUserInput;
-    }
+    
 
     ////////////////////////////
     //  Computational Engine  //
@@ -76,6 +74,7 @@ int main() {
             product = pow(2, k + 1); //Iterative exponentiation in base-2. Runes (k) 22 to 33.
         }
         //idea: create a table "product[k]" and iterate backwards using k-- 
+        //idea: place fgets in a while loop and exit if proper rune name is obtained. Cleaner.
         printf("%s: %.0lf\n", runeTable[k], product);
         k++;
     }
